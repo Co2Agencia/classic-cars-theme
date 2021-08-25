82f360ea1719a505899fd1596ca94d337c897a61
Matias Coco <matiascoco99@gmail.com>
2021-08-25 11:32:17 -0300

    Cambios responsive y background urls

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
         <div class="footer-texto">
             <div class="texto">
                 <span>telefono</span>
-                <p><?php  echo variables_general("tel");?></p>
+                <a href="https://wa.me/<?php  echo variables_general("wpp");?>"><?php  echo variables_general("tel");?></a>
             </div>
 
             <div class="texto">
@@ -22,15 +22,21 @@
                 <i class="fas fa-mail-bulk"></i>
             </a>
 
+            <a href="https://wa.me/<?php  echo variables_general("wpp");?>">
+                <i class="fab fa-whatsapp"></i>
+            </a>
+
         </div>
     </div>
 
     <div class="footer-parte-2">
         <ul class="footer-menu">
+            <li><a href="<?php echo get_permalink( woocommerce_get_page_id( 'shop' ) );?>">Tienda</a></li>
             <li><a href="<?php echo home_url();?>/#sector-contactanos">contacto</a></li>
-            <li><a href="<?php echo home_url();?>/tienda">Buscá</a></li>
-            <li><a href="<?php echo home_url();?>/#sector-vende">vende</a></li>
-            <li><a href="<?php echo home_url();?>/#sector-nosotros">sobre nosotros</a></li>
+            <li><a href="<?php echo home_url();?>">Inicio</a></li>
+            <li><a href="<?php echo get_cat_link_custom("cars");?>">Autos</a></li>
+            <li><a href="<?php echo get_cat_link_custom("bikes");?>">MOTOS</a></li>
+            <li><a href="<?php echo home_url();?>/taller">TALLER</a></li>
         </ul>
 
         <div class="footer-logo">
